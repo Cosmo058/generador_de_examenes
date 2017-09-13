5d8a7db2a2a15a848ad23d97abd6ef3d884dcd71
Angel Sánchez Valencia <angelsanchez058@gmail.com>
2017-09-13 18:12:46 -0500

    Version con las tarjetas, colores y logos correspondientes

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,6 +1,12 @@
 function centrar_tarjetas(){
   var a = $("#preguntas_container").css('width').replace("px","");
-  var b = (a-40)%211;
+  var b = (a-40)%218;
   var c = (b/2)+20;
   $("#preguntas_container").css("padding-left",c+"px");
+}
+
+
+function igualar_altura_logos(){
+  var altura_logo_castillo = $("#logo_castillo_container").css('height');
+  $('#materia_section').css('height',altura_logo_castillo);
 }
