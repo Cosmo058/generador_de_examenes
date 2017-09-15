a001b04ef5b3ee078c895f5659dae908c4d66e1a
Angel Sánchez Valencia <angelsanchez058@gmail.com>
2017-09-14 19:26:20 -0500

    2da pantalla
    Versión que incorpora la segunda pantalla del generador

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -5,8 +5,12 @@ function centrar_tarjetas(){
   $("#preguntas_container").css("padding-left",c+"px");
 }
 
-
 function igualar_altura_logos(){
   var altura_logo_castillo = $("#logo_castillo_container").css('height');
   $('#materia_section').css('height',altura_logo_castillo);
+}
+
+function mostrar_ancho(){
+  var ancho_contenedor = $(".container").css('width');
+  $('#title').html('<h3>Generador de ex&aacute;menes</h3><p>Aprendizajes esperados '+ancho_contenedor+'</p>');
 }
