36e3244d23c51978d3da6f0269c9b13f110684f6
Angel Sánchez Valencia <angelsanchez058@gmail.com>
2017-09-25 18:20:35 -0500

    Agregadas funciones para navegar a través del JSON que contiene los reactivos.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,12 +1,25 @@
 function getObjects(obj, key, val) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getObjects(obj[i], key, val));
-        } else if (i == key && obj[key] == val) {
-            objects.push(obj);
-        }
+  var objects = [];
+  for (var i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] === 'object') {
+      objects = objects.concat(getObjects(obj[i], key, val));
+    } else if (i === key && obj[key] === val) {
+      objects.push(obj);
     }
-    return objects;
+  }
+  return objects;
+}
+
+function getValuesOf(obj, key) {
+  var values = [];
+  for (var i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] === 'object') {
+      values = values.concat(getValuesOf(obj[i], key));
+    } else if (i === key) {
+      values.push(obj[i]);
+    }
+  }
+  return values;
 }
