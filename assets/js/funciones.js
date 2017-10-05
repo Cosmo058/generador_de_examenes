f0b0fcaa8f407a3f2a7bfcaa6ca53f34848af1c9
Angel Sánchez Valencia <angelsanchez058@gmail.com>
2017-10-05 16:12:37 -0500

    Versión preliminar

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -153,3 +153,13 @@ function mostrar_preview_respuestas(opt){
     });
   });
 }
+
+$.fn.toggleAttr = function(attr, attr1, attr2) {
+  return this.each(function() {
+    var self = $(this);
+    if (self.attr(attr) == attr1)
+      self.attr(attr, attr2);
+    else
+      self.attr(attr, attr1);
+  });
+};
